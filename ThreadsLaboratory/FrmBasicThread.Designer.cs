31999377b1729d8cadaed39ae29f4544fd35f67d
Xuee0904 <cedric2827@gmail.com>
2025-09-24 20:03:55 +0800

    Add project files.

---
+++ ThreadsLaboratory/FrmBasicThread.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ThreadsLaboratory
+{
+    partial class FrmBasicThread
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.RunButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(31, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "- Before starting thread -";
+            // 
+            // RunButton
+            // 
+            this.RunButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.RunButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RunButton.Location = new System.Drawing.Point(177, 128);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(131, 50);
+            this.RunButton.TabIndex = 2;
+            this.RunButton.Text = "RUN";
+            this.RunButton.UseVisualStyleBackColor = true;
+            // 
+            // FrmBasicThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 231);
+            this.Controls.Add(this.RunButton);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmBasicThread";
+            this.Text = "BasicThread";
+            this.Load += new System.EventHandler(this.FrmBasicThread_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button RunButton;
+    }
+}
+
---
+++ ThreadsLaboratory/FrmBasicThread.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadsLaboratory
+{
+    public partial class FrmBasicThread : Form
+    {
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+
+            label1.ForeColor = ColorTranslator.FromHtml("#111318");
+            RunButton.BackColor = ColorTranslator.FromHtml("#583722");
+            RunButton.ForeColor = ColorTranslator.FromHtml("#F3F3F3");
+        }
+
+        private void FrmBasicThread_Load(object sender, EventArgs e)
+        {
+            this.BackColor = ColorTranslator.FromHtml("#BDD7DE");
+        }
+ 
+    }
+}
